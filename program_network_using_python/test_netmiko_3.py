c5991d8e5a095ceb7181076f49c23f1eb9ffa365
Quang <quang.hongthien@outlook.com>
2024-05-04 15:36:22 +0700

    add netmiko p3

--- program_network_using_python/test_netmiko_3.py
+++ program_network_using_python/test_netmiko_3.py
@@ -2,7 +2,7 @@ from netmiko import ConnectHandler
 from pprint import pprint
 from inventory.config import inv
 
-ObjFile = open('./tasks/inventory/sw-config', 'r')
+ObjFile = open('./tasks/inventory/netmiko_p3', 'r')
 lines = ObjFile.readlines()
 ObjFile.close()
 pprint(lines)
@@ -18,16 +18,6 @@ for device, info in inv.items():
     output = net_connect.send_command('show ip int brief')
     print(output)
 
-
-# # config_commands = ['int loop 0', 'ip address 1.1.1.1 255.255.255.0']
-# # output = net_connect.send_config_set(config_commands)
-# # print(output)
-
-# output = net_connect.send_command('show vlan')
-# print(output)
-
-# # for n in range (21,22):
-# #     print("Creating VLAN " + str(n))
-# #     config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
-# #     output = net_connect.send_config_set(config_commands)
-# #     print(output)
+    pprint(lines)
+    output = net_connect.send_config_set(lines)
+    print(output)
