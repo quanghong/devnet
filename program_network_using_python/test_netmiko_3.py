ac66e7ffc22a7d772655129a6996564fdde8ed91
Quang <57747218+quanghong@users.noreply.github.com>
2024-05-06 07:41:11 +0000

    update cpu for gns3

--- program_network_using_python/test_netmiko_3.py
+++ program_network_using_python/test_netmiko_3.py
@@ -2,7 +2,7 @@ from netmiko import ConnectHandler
 from pprint import pprint
 from inventory.config import inv
 
-ObjFile = open('./inventory/netmiko_p3', 'r')
+ObjFile = open('./program_network_using_python/inventory/netmiko_p3', 'r')
 lines = ObjFile.readlines()
 ObjFile.close()
 pprint(lines)
@@ -14,10 +14,15 @@ for device, info in inv.items():
     pprint(info)
 
     net_connect = ConnectHandler(**info)
-    # net_connect.find_prompt()
+
     output = net_connect.send_command('show ip int brief')
     print(output)
 
     pprint(lines)
     output = net_connect.send_config_set(lines)
     print(output)
+
+    output = net_connect.send_command('show vlan')
+    print(output)
+
+    break
